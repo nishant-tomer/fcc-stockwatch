1f9a452e60c8f35be86fe908f7b60bcd603c7725
nishant-tomer <nishantfun4@gmail.com>
2016-04-27 16:02:28 +0530

    setup local

--- server.js
+++ server.js
@@ -2,13 +2,12 @@
 var express = require('express');
 
 var app = express()
-
 app.use(express.static( require("path").join(__dirname + "/client")))
 
 app.get("/", function(req,res){
-  
+
   res.sendFile( require("path").join(__dirname + "/client/index.html") )
-  
+
 })
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
